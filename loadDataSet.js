db65812556359ccf77de5881ff9a915a9f450377
Abdul Kadir <114944605+abdulkadir18apr@users.noreply.github.com>
2023-11-19 17:48:22 +0530

    Update loadDataSet.js

--- loadDataSet.js
+++ loadDataSet.js
@@ -1,7 +1,7 @@
 const loadDataSet=()=>{
     const XLSX = require('xlsx');
 const path = require('path');
-
+const { parse } = require('date-fns');
 const excelFilePath = path.join(__dirname, 'data-file/Assignment-Data.xlsx')
 const workbook = XLSX.readFile(excelFilePath);
 
@@ -10,7 +10,12 @@ const sheetName = workbook.SheetNames[0];
 const sheet = workbook.Sheets[sheetName];
 
 
-const data = XLSX.utils.sheet_to_json(sheet);
+
+const data = XLSX.utils.sheet_to_json(sheet,{raw:false});
+data.forEach(row => {
+    const dateValue = row['Day'];
+    row['Date'] = parse(Number(dateValue), { format: 'dd/MM/yyyy', baseDate: new Date(1899, 11, 30) },new Date());
+  });
 return data;
 }
 module.exports=loadDataSet;
